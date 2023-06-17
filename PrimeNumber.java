9529ae1cd0e4878de1ade24c001ca45459f39ed7
Yash1Hingu <yash23hingu@gmail.com>
2023-06-17 18:21:57 +0530

    UPDATE -- isPrime

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -2,20 +2,20 @@ import java.util.*;
 public class PrimeNumber {
     public static void main(String []args) {
         Scanner sc = new Scanner(System.in);
-        boolean flag = false;
+        boolean isPrime = true;
         System.out.print("Enter Number : ");
         int number = sc.nextInt();
         for(int i = 2;i < number-1;i++){
             if(number % i == 0) {
-                flag = true;
+                isPrime = false;
                 break;
             }
         }
 
-        if(flag) {
-            System.out.println("Number is NOT PRIME");
-        } else {
+        if(isPrime) {
             System.out.println("Number is PRIME");
+        } else {
+            System.out.println("Number is NOT PRIME");
         }
     }
 }
