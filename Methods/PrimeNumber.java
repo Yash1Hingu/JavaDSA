7ebd28d60a468151bbb8a69467b44c029de433c2
Yash1Hingu <yash23hingu@gmail.com>
2023-06-18 19:24:44 +0530

    CREATE -- PrimeNumber

---
+++ Methods/PrimeNumber.java
@@ -0,0 +1,30 @@
+package Methods;
+import java.util.*;
+public class PrimeNumber {
+    public static boolean isPrime(int n) {
+        if (n == 0 || n == 1) {
+            return false;
+        } else if (n == 2) {
+            return true;
+        } else {
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(isPrime(n)){
+            System.out.println(n+" is PRIME.");
+        } else {
+            System.out.println(n+" is NOT PRIME.");
+        }
+    }
+}
