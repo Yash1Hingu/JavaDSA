19d79f91d40634389f909fe4779b71a042794a98
Yash1Hingu <yash23hingu@gmail.com>
2023-06-21 18:16:26 +0530

    UPDATE -- binarydecimal function

--- Methods/BinaryToDecimal.java
+++ Methods/BinaryToDecimal.java
@@ -1,20 +1,27 @@
 package Methods;
+
 import java.util.*;
+
 public class BinaryToDecimal {
+
+    public static double binaryDecimal(int n) {
+        double dec = 0, pow = 0;
+        int ld;
+        while (n > 0) {
+            ld = n % 10;
+            dec = dec + (ld * (Math.pow(2, pow)));
+            n = n / 10;
+            pow++;
+        }
+
+        return dec;
+    }
+
     public static void main(String arg[]) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Binary Number : ");
         int n = sc.nextInt();
-        double dec = 0,pow = 0;
-        int ld;
 
-        while(n > 0) {
-            ld = n % 10;
-            dec = dec + (ld*(Math.pow(2,pow)));
-            n = n/10;
-            pow++;
-        }
-
-        System.out.println(dec);
+        System.out.println(binaryDecimal(n));
     }
 }
