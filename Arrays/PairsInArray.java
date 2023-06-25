2d91565bff6ac07ffb489f058264ac4568b7ea70
Yash1Hingu <yash23hingu@gmail.com>
2023-06-25 12:25:02 +0530

    CREATE -- Pairs In Array

---
+++ Arrays/PairsInArray.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class PairsInArray {
+    public static void pairs(int numbers[]) {
+
+        for(int i = 0;i < numbers.length;i++) { 
+            for(int j = i + 1;j < numbers.length;j++) {
+                System.out.print("(" + numbers[i] +","+numbers[j]+")");
+            }
+            System.out.println();
+        } 
+    }
+    public static void main(String args[]) {
+        int numbers[] = {2,4,6,8,10,12,14,16};
+        pairs(numbers);
+    }
+}
