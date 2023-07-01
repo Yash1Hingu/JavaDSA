e19208b3fa51b7fbf952444d7c41f2cf564253eb
Yash1Hingu <yash23hingu@gmail.com>
2023-07-01 17:30:23 +0530

    CREATE -- Strings

---
+++ String/Strings.java
@@ -0,0 +1,39 @@
+package String;
+
+public class Strings {
+    public static void equalString() {
+        String s1 = "Yash";
+        String s2 = "Yash";
+        String s3 = new String("Yash");
+
+        // EQUAL
+        if (s1 == s2) {
+            System.out.println("EQUAL");
+        } else {
+            System.out.println("NOT EQUAL");
+        }
+
+        // NOT EQUAL
+        if (s1 == s3) {
+            System.out.println("EQUAL");
+        } else {
+            System.out.println("NOT EQUAL");
+        }
+
+        if (s1.equals(s3)) {
+            System.out.println("EQUAL");
+        } else {
+            System.out.println("NOT EQUAL");
+        }
+    }
+
+    public static void subString() {
+        String str = "YashHingu";
+        String substr = str.substring(2,6);
+        System.out.println(substr);
+    }
+
+    public static void main(String args[]) {
+        // subString();
+    }
+}
