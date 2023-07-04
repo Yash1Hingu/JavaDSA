09b21f89629e2b2f3893b7cf64b5fe6ff753afcb
Yash1Hingu <yash23hingu@gmail.com>
2023-07-04 21:53:57 +0530

    CREATE -- sum of nature number

---
+++ Recursion/SumOfNatureNo.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class SumOfNatureNo {
+    public static int sum(int n) {
+        if(n == 1) {
+            return 1;
+        }
+        return n + sum(n-1);
+    }
+    public static void main(String args[]) {
+        int n = 10;
+        System.out.println("Sum : "+sum(n));
+    }
+}
