bc3b4bcb5894fabdc148b7c86bcb323e08045f4e
Yash1Hingu <yash23hingu@gmail.com>
2023-07-07 05:17:21 +0530

    MODIFY -- Add Optimize Code Power of X

--- Recursion/PowerofX.java
+++ Recursion/PowerofX.java
@@ -1,6 +1,7 @@
 package Recursion;
 
 public class PowerofX {
+    //O(n)
     public static double power(int x,int n) {
         //Base Condition
         if(n == 1) {
@@ -9,7 +10,20 @@ public class PowerofX {
         //Recursive Call
         return x * power(x,n-1);
     }
+
+    // O(logn)
+    public static int optimizepow(int x,int n) {
+        // Base Case 
+        if(n == 1){
+            return x;
+        }
+        if(n%2 == 0){
+            return optimizepow(x, n/2) * optimizepow(x, n/2);
+        }
+        return x * optimizepow(x, n/2) * optimizepow(x, n/2);
+    }
     public static void main(String args[]) {
-        System.out.println(power(2,10));
+        // System.out.println(power(2,10));
+        System.out.println(optimizepow(2, 10));
     }
 }
