a356fa38eb9a1f8a6a9929fbaf26e4daf0a1dde5
Yash1Hingu <yash23hingu@gmail.com>
2023-08-16 14:29:34 +0530

    ADD -- add methods

--- LinkedList/Linkedlist.java
+++ LinkedList/Linkedlist.java
@@ -12,12 +12,13 @@ public class Linkedlist{
 	
 	public static Node head;
 	public static Node tail;
-
+	public static int size;
 
 	//addFirst
 	public void addFirst(int data){
 		//1. create new node
 		Node newNode = new Node(data);
+		size++;
 
 		//2. no linked list
 		if(head == null){
@@ -36,6 +37,8 @@ public class Linkedlist{
 	public void addLast(int data){
 		// create new node
 		Node newNode = new Node(data);
+		size++;
+
 		if(head == null){
 			head = tail = newNode;
 			return;
@@ -48,12 +51,45 @@ public class Linkedlist{
 		tail = newNode;
 	}
 
+	//Print LinkedList
+	public void print(){ // O(n)
+
+		Node temp = head;
+		while(temp != null){
+			System.out.print(temp.data+" -->");
+			temp = temp.next;
+		}
+		System.out.println("null");
+	}
+
+	//add element on index
+	public void add(int index,int data){
+		if(index == 0){
+			addFirst(data);
+			return;
+		}
+
+		Node temp = head;
+		int i = 0;
+		Node newNode = new Node(data);
+		size++;
+		while(i < index-1){
+			temp = temp.next;
+			i++;
+		}
+		
+		// i = index-1 --> temp = prev
+		newNode.next = temp.next;
+		temp.next = newNode;
+	}
 	public static void main(String args[]){
 		Linkedlist li = new Linkedlist();
 		li.addFirst(1);
 		li.addFirst(2);
 		li.addLast(4);
 		li.addLast(5);
-		System.out.println(tail.data);
+		li.add(3,7);
+		li.print();
+		System.out.println(li.size);
 	}
 }
